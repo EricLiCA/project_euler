4ca030c2fe6b7664585757db1512ef7ee7687456
Eric Li <sida.li@polymtl.ca>
2019-03-28 15:43:55 -0400

    #13: Solved

---
+++ src/011-020/13_large_sum.cpp
@@ -0,0 +1,43 @@
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler013
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+
+auto getData(int n) {
+    std::vector <std::string> nums(n);
+    std::copy_n(std::istream_iterator<std::string>(std::cin), n, nums.begin());
+    return nums;
+}
+
+auto maxDigitNum(int digits) {
+    auto result = 0LL;
+    for (auto i = 0; i < digits; i++) {
+        result *= 10;
+        result += 9;
+    }
+    return result;
+}
+
+auto largeSum(std::vector <std::string> &nums, long long maxDigitsNum, int n) {
+    auto firstDigitsSum = [&](long long digits, std::string &num) {
+        return digits + std::stoll(
+                num.substr(0, log10(maxDigitsNum) + 1 + log10(n)));
+    };
+    auto firstDigits = std::accumulate(nums.begin(), nums.end(), 0LL, firstDigitsSum);
+    while (firstDigits > maxDigitsNum) {
+        firstDigits /= 10;
+    }
+    return firstDigits;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    auto nums = getData(n);
+    std::cout << largeSum(nums, maxDigitNum(10), n) << std::endl;
+}
