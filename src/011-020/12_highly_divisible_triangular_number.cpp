46040bf381027954df13cb562ad3b85bf4ec540d
Eric Li <sida.li@polymtl.ca>
2019-03-28 14:58:25 -0400

    #12 Problem solved with close to optimal algorithm

--- src/011-020/12_highly_divisible_triangular_number.cpp
+++ src/011-020/12_highly_divisible_triangular_number.cpp
@@ -1,15 +1,66 @@
 // https://www.hackerrank.com/contests/projecteuler/challenges/euler012
 
 #include <iostream>
+#include <unordered_map>
+#include <cmath>
 
 auto getData() {
-    long long n;
+    int n;
     std::cin >> n;
     return n;
 }
 
-long long highlyDivisibleTriangularNumber(int n) {
-    return 0LL;
+auto primeFactorCounts(int n) {
+    std::unordered_map<int, unsigned> factorCounts;
+
+    if (n % 2 == 0) {
+        while (n % 2 == 0) {
+            n /= 2;
+            factorCounts[2]++;
+        }
+        // remove one 2 since we divide by 2 to generate the triangle number
+        // triangleNumber = x * (x + 1) / 2
+        factorCounts[2]--;
+    }
+
+    auto limit = sqrt(n);
+    for (int i = 3; i <= limit; i += 2) {
+        while (n % i == 0) {
+            n /= i;
+            factorCounts[i]++;
+        }
+        if (n == 1) break;
+    }
+
+    if (n != 1) factorCounts[n]++;
+
+    return factorCounts;
+}
+
+auto numberOfFactors(std::unordered_map<int, unsigned> factorCounts) {
+    int factors = 1;
+    for (auto &factor: factorCounts) factors *= factor.second + 1;
+    return factors;
+}
+
+auto triangleNumber(int n) {
+    return n * (n + 1) / 2;
+}
+
+auto highlyDivisibleTriangularNumber(int n) {
+    auto x = 1;
+    auto xFactors = numberOfFactors(primeFactorCounts(x));
+    auto xPlusOneFactors = numberOfFactors(primeFactorCounts(x + 1));
+    auto factors = xFactors * xPlusOneFactors;
+
+    while (factors <= n) {
+        x++;
+        xFactors = numberOfFactors(primeFactorCounts(x + 1));
+        std::swap(xFactors, xPlusOneFactors);
+        factors = xFactors * xPlusOneFactors;
+    }
+
+    return triangleNumber(x);
 }
 
 int main() {
